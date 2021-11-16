114c984073a6d41f78197b4fb729de46bf31bc50
Savinoff94 <savinoff94@mail.ru>
2021-11-16 10:20:09 +0200

    first commit

--- emitter/index.js
+++ emitter/index.js
@@ -1,27 +1,20 @@
 function outer(message, storageCapacity, sendingInterval){
-    let messages = [];
-    let timer = 0;
-    let interval = null;
+    const emitterStorage ={
+        messages:[],
+        timer:0,
+        interval:null
+    }
     return function inner(message, storageCapacity, sendingInterval){
-        messages.push(message);
-        if(interval === null){
-            interval = setInterval(function(){
-                ++timer;
-                if(timer >= sendingInterval && messages.length != 0){
-                    console.log('interval');
-                    console.log(messages);
-                    timer = 0;
-                    messages = [];
-                    clearInterval(interval);
-                    interval = null;
+        emitterStorage.messages.push(message);
+        if(emitterStorage.interval === null){
+            emitterStorage.interval = setInterval(function(){
+                ++emitterStorage.timer;
+                if(emitterStorage.timer >= sendingInterval && emitterStorage.messages.length != 0){
+                    sendData(emitterStorage);
                 }
             }, 1000)
-        }else if(messages.length >= storageCapacity){
-            clearInterval(interval);
-            interval = null;
-            console.log(messages);
-            timer = 0;
-            messages = [];
+        }else if(emitterStorage.messages.length >= storageCapacity){
+            sendData(emitterStorage);
         }
     }
 }
@@ -33,6 +26,50 @@ function caller(number){
         emitter('aaa',5,5);
     }
 }
+
+function sendData(obj){
+    console.log(obj.messages);
+    obj.timer = 0;
+    obj.messages = [];
+    clearInterval(obj.interval);
+    obj.interval = null;
+}
+
+// function outer(message, storageCapacity, sendingInterval){
+//     let messages = [];
+//     let timer = 0;
+//     let interval = null;
+//     return function inner(message, storageCapacity, sendingInterval){
+//         messages.push(message);
+//         if(interval === null){
+//             interval = setInterval(function(){
+//                 ++timer;
+//                 if(timer >= sendingInterval && messages.length != 0){
+//                     // console.log('interval');
+//                     console.log(messages);
+//                     timer = 0;
+//                     messages = [];
+//                     clearInterval(interval);
+//                     interval = null;
+//                 }
+//             }, 1000)
+//         }else if(messages.length >= storageCapacity){
+//             clearInterval(interval);
+//             interval = null;
+//             console.log(messages);
+//             timer = 0;
+//             messages = [];
+//         }
+//     }
+// }
+
+// let emitter = outer();
+
+// function caller(number){
+//     for(let i = 0; i < number; ++i){
+//         emitter('aaa',5,5);
+//     }
+// }
 
 // function outer(message, storageCapacity, sendingInterval){
 //     let messages = [];
@@ -77,3 +114,41 @@ function caller(number){
 //         }
 //     }
 // }
+
+
+function outer(message, storageCapacity, sendingInterval){
+    const emitterStorage ={
+        messages:[],
+        timer:0,
+        interval:null
+    }
+    return function inner(message, storageCapacity, sendingInterval){
+        emitterStorage.messages.push(message);
+        if(emitterStorage.interval === null){
+            emitterStorage.interval = setInterval(function(){
+                ++emitterStorage.timer;
+                if(emitterStorage.timer >= sendingInterval && emitterStorage.messages.length != 0){
+                    sendData(emitterStorage);
+                }
+            }, 1000)
+        }else if(emitterStorage.messages.length >= storageCapacity){
+            sendData(emitterStorage);
+        }
+    }
+}
+
+let emitter = outer();
+
+function caller(number){
+    for(let i = 0; i < number; ++i){
+        emitter('aaa',5,5);
+    }
+}
+
+function sendData(obj){
+    console.log(obj.messages);
+    obj.timer = 0;
+    obj.messages = [];
+    clearInterval(obj.interval);
+    obj.interval = null;
+}
